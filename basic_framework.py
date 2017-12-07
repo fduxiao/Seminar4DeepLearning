923d0e782247e2eecfff583fed24b68882c1909a
fduxiao <xiao.tan@qianpuinvestment.com>
2017-12-07 17:45:33 +0800

    basic function

---
+++ basic_framework.py
@@ -0,0 +1,111 @@
+import tensorflow as tf
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def conv2d(x, w):
+    return tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x, name=None):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
+
+
+def variable_summaries(var):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
+
+def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
+    """Reusable code for making a simple neural net layer.
+
+    It does a matrix multiply, bias add, and then uses relu to nonlinearize.
+    It also sets up name scoping so that the resultant graph is easy to read,
+    and adds a number of summary ops.
+    """
+    # Adding a name scope ensures logical grouping of the layers in the graph.
+    with tf.name_scope(layer_name):
+        # This Variable will hold the state of the weights for the layer
+        with tf.name_scope('weights'):
+            weights = weight_variable([input_dim, output_dim])
+            variable_summaries(weights)
+        with tf.name_scope('biases'):
+            biases = bias_variable([output_dim])
+            variable_summaries(biases)
+        with tf.name_scope('Wx_plus_b'):
+            preactivate = tf.matmul(input_tensor, weights) + biases
+            tf.summary.histogram('pre_activations', preactivate)
+        activations = act(preactivate, name='activation')
+        tf.summary.histogram('activations', activations)
+        return activations
+
+
+def cnn_layer(input_tensor, conv_dim, layer_name, act=tf.nn.relu):
+    with tf.name_scope(layer_name):
+        # This Variable will hold the state of the weights for the layer
+        with tf.name_scope('conv_kernel'):
+            weights = weight_variable(conv_dim)
+            variable_summaries(weights)
+        with tf.name_scope('conv_biases'):
+            biases = bias_variable([conv_dim[-1]])
+            variable_summaries(biases)
+        with tf.name_scope('conv_Wx_plus_b'):
+            conv_preactivate = tf.nn.relu(conv2d(input_tensor, weights) + biases)
+            tf.summary.histogram('pre_activations', conv_preactivate)
+        activations = act(conv_preactivate, name='activation')
+        tf.summary.histogram('activations', activations)
+        pooled = max_pool_2x2(activations, name='pooled')
+        tf.summary.histogram('pooled', pooled)
+        return pooled
+
+
+def le_net(input_tensor, keep_prob, net_name):
+    with tf.name_scope(net_name):
+        x_image = tf.reshape(input_tensor, [-1, 28, 28, 1])
+        cnn1 = cnn_layer(x_image, [5, 5, 1, 32], "cnn1")
+        cnn2 = cnn_layer(cnn1, [5, 5, 32, 64], "cnn2")
+        cnn2_flat = tf.reshape(cnn2, [-1, 7 * 7 * 64])
+
+        nn1 = nn_layer(cnn2_flat, 7 * 7 * 64, 1024, 'nn1')
+        nn1_drop = tf.nn.dropout(nn1, keep_prob)
+
+        nn2 = nn_layer(nn1_drop, 1024, 10, 'nn2')
+        nn2_drop = tf.nn.dropout(nn2, keep_prob)
+    return nn2_drop
+
+
+def train_affair(labels, logits, name):
+    with tf.name_scope(name):
+        with tf.name_scope('cross_entropy'):
+            diff = tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+            with tf.name_scope('total'):
+                cross_entropy = tf.reduce_mean(diff)
+        tf.summary.scalar('cross_entropy', cross_entropy)
+
+        with tf.name_scope('train'):
+            train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+        with tf.name_scope('accuracy'):
+            with tf.name_scope('correct_prediction'):
+                correct_prediction = tf.equal(tf.argmax(labels, 1), tf.argmax(logits, 1))
+            with tf.name_scope('accuracy'):
+                accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar('accuracy', accuracy)
+
+    return train_step, accuracy
---
+++ normal.py
@@ -0,0 +1,52 @@
+from basic_framework import *
+from tensorflow.examples.tutorials.mnist import input_data
+import os
+
+
+def main():
+
+    with tf.name_scope('normal'):
+        y_ = tf.placeholder(tf.float32, [None, 10], name='y_')
+        x = tf.placeholder(tf.float32, [None, 784], name='x')
+        nstep = tf.Variable(0, trainable=False, name='step')
+        with tf.name_scope('dropout'):
+            keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+            tf.summary.scalar('dropout_keep_probability', keep_prob)
+
+        main_net = le_net(x, keep_prob, 'LeNet')
+        train_step, accuracy = train_affair(y_, main_net, 'train_affair')
+
+    merged = tf.summary.merge_all()
+
+    sess = tf.Session()
+    mnist = input_data.read_data_sets("./MNIST_data/", one_hot=True)
+
+    train_writer = tf.summary.FileWriter('./tensorboard/normal' + '/train', sess.graph)
+    saver = tf.train.Saver()
+    saver_path = './checkpoints/normal.ckpt'
+    if os.path.isfile(saver_path+'.meta'):
+        saver.restore(sess, saver_path)
+    else:
+        init_op = tf.global_variables_initializer()
+        sess.run(init_op)
+
+    start = sess.run(nstep)
+    for i in range(start, 20000):
+        batch = mnist.train.next_batch(50)
+        if i % 100 == 0:
+            train_accuracy = sess.run(accuracy, feed_dict={
+                x: batch[0], y_: batch[1], keep_prob: 1.0})
+            print("step %d, training accuracy %g" % (i, train_accuracy))
+        if i % 500 == 0:
+            sess.run(tf.assign(nstep, i))
+            saver.save(sess, "./checkpoints/normal.ckpt")
+            saver.save(sess, './checkpoints/normal_%d.ckpt' % i)
+        summary, _ = sess.run([merged, train_step], feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+        train_writer.add_summary(summary, i)
+
+    print("test accuracy %g" % sess.run(accuracy, feed_dict={
+        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+
+if __name__ == '__main__':
+    main()
